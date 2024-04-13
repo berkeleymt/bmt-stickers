7ed52f68243e4b7476340737e4b4ec5be9b7a0fe
Oliver Ni <oliver.ni@gmail.com>
2024-04-13 13:32:43 -0700

    Update for bmmt 2024

--- generate.py
+++ generate.py
@@ -1,80 +1,31 @@
 import os
 import sys
 import tempfile
+from multiprocessing import Pool
 from pathlib import Path
 from subprocess import check_call
 
 import requests
 
-EVENT_ID = "iQUgJ5g4cOyUff7sqP7C"
+EVENT_ID = "IY75fFIx7257tFDBa4yp"
 
 SHEET = """
-#sheet(
+sheet(
   org: "{org_name}",
   team: "{team_name}",
   number: "{team_number}",
   rooms: (
-    power: "{power_room}",
-    guts: "{guts_room}",
+    exam: "{exam_room}",
     awards: "{awards_room}",
   ),
   students: {students},
-)
+),
 """
 
-AWARDS_DWINELLE_145 = {
-    "ar8cJSArZ5P2cSstO78w",
-    "lI2FYvPdAlRqN098FLBr",
-    "r8IQdZlEZ1rDvrRCEIBR",
-    "5tQWdEHW9aBC1mZ8dBfE",
-    "fXyxweZJsTnISQseSXhr",
-    "e1mm0BLo1G6UnAyte5Tw",
-    "CkyzuKre4VIT7pltGLyj",
-    "NDSyHT2K6vkkXtD5d0qv",
-    "2DZYstMox830PYxGoYZ5",
-    "cjFRU5YxcaSl4N1P6phX",
-    "OBZBUMt3RCdUld4T3Oli",
-    "8KJeCQPaknYoFLEMTHs3",
-    "X6M65di7Dm5p5iQhVrmP",
-    "ijZR7mVr1ClTsx3Hrtxa",
-    "6hHPLebHPVoNBEVoQAyV",
-    "h6Ncdgtxvx2XZ09H3mGC",
-    "SYM8zPeh9F6MVQTz9ejD",
-    "HdQQrzP0RHBr39iovqQu",
-    "wMmBNEEArJSe3L6asrId",
-    "8eAzIBDcLu5MDHaR5Heu",
-    "gP7eoEtC0juTiJ5E6KtB",
-    "fa4CsVK5FO2sjFf0fiTQ",
-    "qmPujuqTdvAUdIjC9IGT",
-    "BBMA4JOdTXExxZ2kl6bF",
-    "lwbWvhSfVtaxIgccB9D4",
-}
-
-GUTS_ROOMS = {
-    "Power Room: Dwinelle 109": "Dwinelle 155",
-    "Power Room: Dwinelle 130": "Dwinelle 155",
-    "Power Room: Dwinelle 179": "Dwinelle 155",
-    "Power Room: Dwinelle 182": "Dwinelle 155",
-    "Power Room: Dwinelle 183": "Dwinelle 155",
-    "Power Room: Dwinelle 187": "Dwinelle 155",
-    "Power Room: Dwinelle 205": "Dwinelle 155",
-    "Power Room: Dwinelle 206": "Dwinelle 155",
-    "Power Room: Dwinelle 255": "Dwinelle 145",
-    "Power Room: Dwinelle 258": "Dwinelle 145",
-    "Power Room: Dwinelle 259": "Dwinelle 145",
-    "Power Room: Dwinelle 262": "Dwinelle 145",
-    "Power Room: Dwinelle 263": "Dwinelle 145",
-    "Power Room: Dwinelle 283": "Dwinelle 145",
-    "Power Room: Dwinelle 79": "Dwinelle 155",
-    "Power Room: Dwinelle 83": "Dwinelle 155",
-    "Power Room: Dwinelle 87": "Dwinelle 155",
-    "Power Room: Dwinelle 89": "Dwinelle 155",
-    "Power Room: SOCS 136": "Hearst Field Annex A1",
-    "Power Room: SOCS 151": "Hearst Field Annex A1",
-    "Power Room: SOCS 155": "Hearst Field Annex A1",
-    "Power Room: SOCS 174": "Hearst Field Annex A1",
-    "Power Room: SOCS 175": "Hearst Field Annex A1",
-    "Power Room: SOCS 185": "Hearst Field Annex A1",
+AWARDS = {
+    ("A", "C"): "Dwinelle 145",
+    ("C", "Re"): "Dwinelle 155",
+    ("Re", "Z"): "Hearst Field Annex A1",
 }
 
 SESSION = requests.Session()
@@ -82,41 +33,59 @@ SESSION.headers = {"Authorization": f"Bearer {os.environ['CONTESTDOJO_API_KEY']}
 
 
 def make_sheet(number):
-    team_req = SESSION.get(f"https://api.contestdojo.com/events/{EVENT_ID}/teams/?number={number}")
+    team_req = SESSION.get(
+        f"https://api.contestdojo.com/events/{EVENT_ID}/teams/?number={number}"
+    )
     team_req.raise_for_status()
     try:
         team_data = team_req.json()[0]
     except IndexError:
         raise ValueError(f"Team {number} not found")
 
-    org_req = SESSION.get(f"https://api.contestdojo.com/events/{EVENT_ID}/orgs/{team_data['org']}")
+    org_req = SESSION.get(
+        f"https://api.contestdojo.com/events/{EVENT_ID}/orgs/{team_data['org']}"
+    )
     org_req.raise_for_status()
     org_data = org_req.json()
 
-    students_req = SESSION.get(f"https://api.contestdojo.com/events/{EVENT_ID}/students/?team={team_data['id']}")
+    students_req = SESSION.get(
+        f"https://api.contestdojo.com/events/{EVENT_ID}/students/?team={team_data['id']}"
+    )
     students_req.raise_for_status()
     students_data = students_req.json()
 
     students = "\n".join(
-        f'(number: "{student["number"]}", name: "{student["fname"]} {student["lname"]}"),' for student in students_data
+        f'(number: "{student["number"]}", name: "{student["fname"]} {student["lname"]}"),'
+        for student in students_data
     )
+
+    for (s, e), awards_room in AWARDS.items():
+        if s <= org_data["name"] <= e:
+            break
 
     return SHEET.format(
         org_name=org_data["name"],
         team_name=team_data["name"],
         team_number=team_data["number"],
-        power_room=team_data["checkInPool"],
-        guts_room=GUTS_ROOMS[team_data["checkInPool"]],
-        awards_room="Dwinelle 145" if org_data["id"] in AWARDS_DWINELLE_145 else "Dwinelle 155",
+        exam_room=team_data["checkInPool"],
+        awards_room=awards_room,
         students=f"({students})",
     )
 
 
 if __name__ == "__main__":
     numbers = input("Team #s: ").replace(",", " ").split()
-    sheets = "\n\n".join(make_sheet(team) for team in numbers)
+    # sheets = [make_sheet(team) for team in numbers]
+    with Pool() as pool:
+        sheets = pool.map(make_sheet, numbers)
+    sheets = "\n\n".join(sheets)
 
-    typst_code = f'#import "template.typ": sheet\n\n{sheets}'
+    typst_code = f"""
+        #import "template.typ": wrapper, sheets, sheet
+        #wrapper(sheets(
+            {sheets}
+        ))
+    """
 
     fd, filename = tempfile.mkstemp("w", dir=Path())
     fp = open(fd, "w")
--- template.typ
+++ template.typ
@@ -1,3 +1,11 @@
+#let wrapper(body) = {
+  set page(paper: "us-letter", margin: (x: 0.156in, y: 0.5in))
+  set text(font: "IBM Plex Sans", hyphenate: true, lang: "en", size: 10pt)
+  show par: set block(above: 0.8em, below: 0.8em)
+  show heading: set block(above: 0.8em, below: 0.9em)
+  body
+}
+
 #let _sticker(content) = box(
   // stroke: 1pt,
   width: 4in,
@@ -8,24 +16,21 @@
 
 #let sticker(content) = _sticker(align(center + horizon, content))
 
+#let dotdotdot = box(width: 1fr, text(fill: gray, repeat("·")))
+
 #let sheet(
-  org: "-",
-  team: "-",
-  number: "-",
-  rooms: (power: "-", guts: "-", awards: "-"),
+  org: "—",
+  team: "—",
+  number: "—",
+  rooms: (exam: "—", awards: "—"),
   students: ()
 ) = {
-  set page(paper: "us-letter", margin: (x: 0.156in, y: 0.5in))
-  set text(hyphenate: true, lang: "en")
-  show par: set block(above: 1em, below: 1em)
-  show heading: set block(above: 1em, below: 1em)
-
   let indiv(student) = _sticker[
     #stack(
       dir: ltr,
       spacing: 1fr,
-      align(bottom, text(size: 2.5em, weight: "bold", student.number)),
-      align(bottom, image("bmt black.png", height: 2em)),
+      align(horizon, text(size: 2em, weight: "bold")[ID: #student.number]),
+      align(horizon, image("bmt black.png", height: 1.6em)),
     )
 
     #line(length: 100%)
@@ -34,8 +39,10 @@
       = #student.name
 
       #text(size: 1.2em)[
-        _#(org)_ \
-        _#number • #(team)_
+        Organization #dotdotdot _#(org)_ \
+        Team         #dotdotdot _#(team)_ (ID: #number) \
+        Exam Room    #dotdotdot _#(rooms.exam)_ \
+        Awards Room  #dotdotdot _#(rooms.awards)_
       ]
     ]
   ]
@@ -49,34 +56,52 @@
   ]
 
   grid(
-    columns: 2,
+    columns: 1,
     column-gutter: 0.188in,
-    ..students.map(student => indiv(student)),
-    ..range(students.len(), 6).map(_ => sticker(image("bmt gray.png", height: 4em))),
-    room[
-      *Power Room* \
-      #rooms.power
-    ],
-    room[
-      *Guts Room* \
-      #rooms.guts
-    ],
-    room[
-      *Awards Room* \
-      #rooms.awards
-    ],
-    sticker[
-      *#number • #team* \
-      _#(org)_
-
-      #table(
-        columns: (1fr, 1.4fr, 2.4fr),
-        [*Time*], [*Event*], [*Room Assignment* ],
-        [9:00 AM], [Power Round], rooms.power,
-        [10:45 AM], [Focus Rounds], [_Check Tournament Guide_],
-        [4:00 PM], [Guts Round], rooms.guts,
-        [6:00 PM], [Awards], rooms.awards,
-      )
-    ],
+    ..range(5).map(idx =>
+      if idx < students.len() {
+        indiv(students.at(idx))
+      } else {
+        sticker(image("bmt gray.png", height: 4em))
+      },
+    ),
   )
 }
+
+#let sheets(..args) = grid(
+  columns: 2,
+  column-gutter: 0.188in,
+  ..args,
+)
+
+// #sheets(
+//   sheet(
+//     org: "Moor High School",
+//     team: "Black",
+//     number: "102",
+//     rooms: (exam: "Dwinelle 100", awards: "Dwinelle 145"),
+//     students: (
+//       (name: "Moor Xu", number: "102A"),
+//       (name: "Andrew Liu", number: "102B"),
+//       (name: "Oliver Ni", number: "102C"),
+//       (name: "Brian Su", number: "102D"),
+//       (name: "Danielle Murphy", number: "102E"),
+//     )
+//   ),
+//   sheet(
+//     org: "Moor High School",
+//     team: "Black",
+//     number: "102",
+//     rooms: (exam: "Dwinelle 100", awards: "Dwinelle 145"),
+//     students: (
+//       (name: "Danielle Murphy", number: "102E"),
+//       (name: "Brian Su", number: "102D"),
+//       (name: "Oliver Ni", number: "102C"),
+//       (name: "Andrew Liu", number: "102B"),
+//       (name: "Moor Xu", number: "102A"),
+//     )
+//   )
+// )
+
+
+
